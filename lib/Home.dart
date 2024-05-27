190c8e7bfd8a748301f17d78b9b8369578f5ff13
Robério <rroberiosouza@gmail.com>
2024-05-26 23:02:53 -0300

    Refatorado projeto para que o usuário consiga interagir melhor com o App e saber se acertou ou não.

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+import 'dart:math';
+
 import 'package:cara_coroa/Jogando.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -9,6 +13,27 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String _escolhaJogador = "";
+  String _avisoUsuario = "";
+  String _moedaAppEscolheu = "";
+  int _indice = -1;
+
+  void limpaEscolhaJogador() {
+    setState(() {
+      _escolhaJogador = "";
+      _avisoUsuario = "";
+    });
+  }
+
+  String _sortear() {
+    var moedas = ['images/moeda_cara.png', 'images/moeda_coroa.png'];
+    setState(() {
+      _indice = Random().nextInt(moedas.length);
+      _moedaAppEscolheu = moedas[_indice];
+    });
+    return _moedaAppEscolheu;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,15 +47,92 @@ class _HomeState extends State<Home> {
               child: Column(
                 children: [
                   Padding(
-                    padding: EdgeInsets.only(bottom: 50),
-                    child: Image.asset("images/logo.png"),
+                    padding: EdgeInsets.only(bottom: 20),
+                    child: Image.asset(
+                      "images/logo.png",
+                      width: 200,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              _escolhaJogador = "cara";
+                              _avisoUsuario = "";
+                            });
+                          },
+                          child: Container(
+                            child: Image.asset(
+                              "images/moeda_cara.png",
+                              width: 150,
+                            ),
+                            decoration: BoxDecoration(
+                                border: (_escolhaJogador == "cara")
+                                    ? Border.all(width: 5, color: Colors.red)
+                                    : null,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(5))),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              _escolhaJogador = "coroa";
+                              _avisoUsuario = "";
+                            });
+                          },
+                          child: Container(
+                            child: Image.asset(
+                              "images/moeda_coroa.png",
+                              width: 150,
+                            ),
+                            decoration: BoxDecoration(
+                                border: (_escolhaJogador == "coroa")
+                                    ? Border.all(width: 5, color: Colors.red)
+                                    : null,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(5))),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 15),
+                    child: Text(
+                      _avisoUsuario,
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
                   ),
                   GestureDetector(
                     onTap: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Jogando()));
+                      if (_escolhaJogador != "") {
+                        _sortear();
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Jogando(
+                                      escolhaUsuario: _escolhaJogador,
+                                      moeda: _moedaAppEscolheu,
+                                      indice: _indice,
+                                    ))).then((rs) => limpaEscolhaJogador());
+                      } else {
+                        setState(() {
+                          _avisoUsuario = "Selecione uma das opções acima.";
+                        });
+                      }
                     },
-                    child: Image.asset("images/botao_jogar.png"),
+                    child: Image.asset(
+                      "images/botao_jogar.png",
+                      width: 150,
+                    ),
                   )
                 ],
               ),
--- lib/Jogando.dart
+++ lib/Jogando.dart
@@ -1,23 +1,26 @@
-import 'dart:math';
-
-import 'package:cara_coroa/Home.dart';
 import 'package:flutter/material.dart';
 
 class Jogando extends StatefulWidget {
-  const Jogando({super.key});
+  final String escolhaUsuario;
+  final String moeda;
+  final int indice;
+  const Jogando(
+      {super.key,
+      required this.escolhaUsuario,
+      required this.moeda,
+      required this.indice});
 
   @override
   State<Jogando> createState() => _JogandoState();
 }
 
 class _JogandoState extends State<Jogando> {
-  String? _moeda;
-
-  String _sortear() {
-    var moedas = ['images/moeda_cara.png', 'images/moeda_coroa.png'];
-    _moeda = moedas[Random().nextInt(moedas.length)];
-
-    return _moeda!;
+  bool _acertou(String escolhaUsuario, int indice) {
+    if (((escolhaUsuario == "cara") && (indice == 0)) ||
+        ((escolhaUsuario == "coroa") && (indice == 1))) {
+      return true;
+    }
+    return false;
   }
 
   @override
@@ -34,13 +37,31 @@ class _JogandoState extends State<Jogando> {
                 children: [
                   Padding(
                     padding: EdgeInsets.only(bottom: 20),
-                    child: Image.asset(_sortear()),
+                    child: Image.asset(
+                      widget.moeda,
+                      width: 150,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 10),
+                    child: Text(
+                      (_acertou(widget.escolhaUsuario, widget.indice))
+                          ? "Você acertou!"
+                          : "Que pena, tente novamente.",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
                   ),
                   GestureDetector(
                     onTap: () {
                       Navigator.pop(context);
                     },
-                    child: Image.asset("images/botao_voltar.png"),
+                    child: Image.asset(
+                      "images/botao_voltar.png",
+                      width: 150,
+                    ),
                   )
                 ],
               ),
